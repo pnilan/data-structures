fcf57c3d01bfe94547657b8118a01019eb17923d
Patrick Nilan <nilan.patrick@gmail.com>
2023-05-17 12:17:35 -0700

    function-shared stack passes all tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {
+    storage: {},
+    push: stackMethods.push,
+    pop: stackMethods.pop,
+    size: stackMethods.size
+  };
+
+  return instance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[Object.keys(this.storage).length] = value;
+
+};
+
+stackMethods.pop = function() {
+  var keys = Object.keys(this.storage);
+  var tempValue = this.storage[keys.length - 1];
+  delete this.storage[keys.length - 1];
+  return tempValue;
+
+};
+
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
 
 
