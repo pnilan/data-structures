a2b2b09cf9b0ec20e4d81f86acd09c6c6b65d562
Patrick Nilan <nilan.patrick@gmail.com>
2023-05-17 11:56:54 -0700

    functional stack works

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,18 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[Object.keys(storage).length] = value;
   };
 
   someInstance.pop = function() {
+    var keys = Object.keys(storage);
+    var temp = storage[keys[keys.length - 1]];
+    delete storage[keys[keys.length - 1]];
+    return temp;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
