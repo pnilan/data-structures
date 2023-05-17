9a57de84a1397bcb4858bb8db0afa915e0b9dac2
Patrick Nilan <nilan.patrick@gmail.com>
2023-05-17 15:43:34 -0700

    es6 stack.js passes all tests

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,19 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    this.storage[Object.keys(this.storage).length] = value;
+  }
+
+  pop() {
+    var tempValue = this.storage[Object.keys(this.storage).length - 1];
+    delete this.storage[Object.keys(this.storage).length - 1];
+    return tempValue;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
