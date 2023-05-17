c0908ffa74a1c456419be4f04bc406044873bbe7
Patrick Nilan <nilan.patrick@gmail.com>
2023-05-17 12:06:04 -0700

    functional queue passes all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,40 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var storageKeys = Object.keys(storage);
+    var maxKey = 0;
+
+    for (var key in storage) {
+      if (maxKey === undefined) {
+        maxKey = Number(key);
+      } else if (Number(key) > maxKey) {
+        maxKey = Number(key);
+      }
+    }
+
+    storage[maxKey + 1] = value;
   };
 
   someInstance.dequeue = function() {
+    var storageKeys = Object.keys(storage);
+
+    var lowestKey;
+    for (var key in storage) {
+
+      if (lowestKey === undefined) {
+        lowestKey = Number(key);
+      } else if (Number(key) < lowestKey) {
+        lowestKey = Number(key);
+      }
+    }
+
+    var temp = storage[lowestKey];
+    delete storage[lowestKey];
+    return temp;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
