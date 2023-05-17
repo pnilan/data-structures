84f1ddcf1bd15e940847a0df4a46a0582f995d49
Patrick Nilan <nilan.patrick@gmail.com>
2023-05-17 15:10:06 -0700

    prototypal stack.js passes all tests

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.storage = {};
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[Object.keys(this.storage).length - 1] = value;
+  },
+  pop: function() {
+    var maxKey;
+    for (var key in this.storage) {
+      if (maxKey === undefined || maxKey < Number(key)) {
+        maxKey = Number(key);
+      }
+    }
+
+    var tempValue = this.storage[maxKey];
+    delete this.storage[maxKey];
+    return tempValue;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
