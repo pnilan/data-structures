91401b3e7f8573431c29a97c7c9f8828be944963
Patrick Nilan <nilan.patrick@gmail.com>
2023-05-17 15:38:13 -0700

    psuedoclassical queue.js passes all tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,34 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+};
+
+Queue.prototype.enqueue = function(value) {
+  var maxKey = 0;
+  for (var key in this.storage) {
+    if (Number(key) > maxKey) {
+      maxKey = Number(key);
+    }
+  }
+
+  this.storage[maxKey + 1] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  var minKey;
+  for (var key in this.storage) {
+    if (minKey === undefined || Number(key) < minKey) {
+      minKey = Number(key);
+    }
+  }
+
+  var tempValue = this.storage[minKey];
+  delete this.storage[minKey];
+  return tempValue;
+
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
 };
 
 
